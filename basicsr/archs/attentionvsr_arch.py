e5063134d4447fb8c66aff027e0206df04792935
Kimsure <54945340+Kimsure@users.noreply.github.com>
2022-11-05 14:29:19 +0800

    Update attentionvsr_arch.py

--- basicsr/archs/attentionvsr_arch.py
+++ basicsr/archs/attentionvsr_arch.py
@@ -69,25 +69,21 @@ class BasicAttention_VSR(nn.Module):
 
     def get_attention(self, x):
         b, n, c, h, w = x.size()
-        # extract features for each frame
-        feat = self.lrelu(self.conv_first(x.view(-1, c, h, w)))  # [B*5, 64, 64, 64]
+        feat = self.lrelu(self.conv_first(x.view(-1, c, h, w))) 
         # print(feat.shape)
-        feat = self.feature_extraction(feat).view(b, n, -1, h, w)  # [B, 5, 64, 64, 64]
+        feat = self.feature_extraction(feat).view(b, n, -1, h, w) 
         print(feat.shape)
         # transformer
 
-        feat = feat + self.pos_embedding(feat)  # [B, 5, 64, 64, 64]
-        # tr_feat = self.transformer(feat)                                 # [B, 5, 64, 64, 64]
-        attention_feat = self.transformer(feat)  # [B, 5, 64, 64, 64]
-        # attention_feat = tr_feat.view(b, -1, h, w)                     # [B, n*, 64, 64]
-        # print(attention_feat.shape)
+        feat = feat + self.pos_embedding(feat)                              
+        attention_feat = self.transformer(feat)  
         return attention_feat
 
     def forward(self, x):
         flows_forward, flows_backward = self.get_flow(x)
         b, n, c, h, w = x.size()
         attention_feat = self.get_attention(x)
-        # backward branch
+        # branch
         out_l = []
         feat_prop = x.new_zeros(b, self.num_feat, h, w)
         for i in range(n - 1, -1, -1):
@@ -95,25 +91,28 @@ class BasicAttention_VSR(nn.Module):
             # print('test_backwardbranch')
             # print(x_i.shape)
             if i < n - 1:
-                flow = flows_backward[:, i, :, :, :]
-                feat_prop = flow_warp(feat_prop, flow.permute(0, 2, 3, 1))
-            feat_prop = torch.cat([x_i, feat_prop], dim=1)
+                flow1 = flows_backward[:, i, :, :, :]
+                flow2 = flows_forward[:, i, :, :, :]
+                feat_prop1 = flow_warp(feat_prop, flow1.permute(0, 2, 3, 1))
+                feat_prop2 = flow_warp(feat_prop, flow2.permute(0, 2, 3, 1))
+            feat_prop = torch.cat([x_i, feat_prop1, feat_prop2], dim=1)
             feat_prop = self.backward_trunk(feat_prop)
             out_l.insert(0, feat_prop)
-        out_o = torch.stack(out_l, dim=1)  # [b, 14, 64, 64, 64] tensor
-        out_o = torch.cat([out_o, attention_feat], dim=2)  # [b, 14, 128, 64, 64] tensor
-        # print('out_o shape')
+        out_o = torch.stack(out_l, dim=1) 
+        out_o = torch.cat([out_o, attention_feat], dim=2)  
         # print(out_o.shape)
 
-        # forward branch
+        # refine
         feat_prop = torch.zeros_like(feat_prop)
         for i in range(0, n):
             x_i = x[:, i, :, :, :]
             if i > 0:
-                flow = flows_forward[:, i - 1, :, :, :]
-                feat_prop = flow_warp(feat_prop, flow.permute(0, 2, 3, 1))
+                flow1 = flows_forward[:, i - 1, :, :, :]
+                flow2 = flows_backward[:, i - 1, :, :, :]
+                feat_prop1 = flow_warp(feat_prop, flow1.permute(0, 2, 3, 1))
+                feat_prop2 = flow_warp(feat_prop, flow2.permute(0, 2, 3, 1))
 
-            feat_prop = torch.cat([x_i, feat_prop], dim=1)
+            feat_prop = torch.cat([x_i, feat_prop1, feat_prop2], dim=1)
             feat_prop = self.forward_trunk(feat_prop)
 
             # upsample
@@ -126,7 +125,7 @@ class BasicAttention_VSR(nn.Module):
             base = F.interpolate(x_i, scale_factor=4, mode='bilinear', align_corners=False)
             out += base
             out_l[i] = out
-        outo = torch.stack(out_l, dim=1)
+        # outo = torch.stack(out_l, dim=1)
         # print(outo.shape)
 
         return torch.stack(out_l, dim=1)
@@ -188,54 +187,42 @@ class globalAttention(nn.Module):
         self.dim = patch_size ** 2 * num_feat
         self.hidden_dim = self.dim // heads
         self.num_patch = (64 // patch_size) ** 2
-
         self.to_q = nn.Conv2d(in_channels=num_feat, out_channels=num_feat, kernel_size=3, padding=1, groups=num_feat)
         self.to_k = nn.Conv2d(in_channels=num_feat, out_channels=num_feat, kernel_size=3, padding=1, groups=num_feat)
         self.to_v = nn.Conv2d(in_channels=num_feat, out_channels=num_feat, kernel_size=3, padding=1)
-
         self.conv = nn.Conv2d(in_channels=num_feat, out_channels=num_feat, kernel_size=3, padding=1)
-
         self.feat2patch = torch.nn.Unfold(kernel_size=patch_size, padding=0, stride=patch_size)
         self.patch2feat = torch.nn.Fold(output_size=(64, 64), kernel_size=patch_size, padding=0, stride=patch_size)
 
     def forward(self, x):
-        b, t, c, h, w = x.shape  # B, 5, 64, 64, 64
+        b, t, c, h, w = x.shape  
         H, D = self.heads, self.dim
         n, d = self.num_patch, self.hidden_dim
-
-        q = self.to_q(x.view(-1, c, h, w))  # [B*5, 64, 64, 64]
-        k = self.to_k(x.view(-1, c, h, w))  # [B*5, 64, 64, 64]
-        v = self.to_v(x.view(-1, c, h, w))  # [B*5, 64, 64, 64]
-
-        unfold_q = self.feat2patch(q)  # [B*5, 8*8*64, 8*8]
-        unfold_k = self.feat2patch(k)  # [B*5, 8*8*64, 8*8]
-        unfold_v = self.feat2patch(v)  # [B*5, 8*8*64, 8*8]
-
-        unfold_q = unfold_q.view(b, t, H, d, n)  # [B, 5, H, 8*8*64/H, 8*8]
-        unfold_k = unfold_k.view(b, t, H, d, n)  # [B, 5, H, 8*8*64/H, 8*8]
-        unfold_v = unfold_v.view(b, t, H, d, n)  # [B, 5, H, 8*8*64/H, 8*8]
-
-        unfold_q = unfold_q.permute(0, 2, 3, 1, 4).contiguous()  # [B, H, 8*8*64/H, 5, 8*8]
-        unfold_k = unfold_k.permute(0, 2, 3, 1, 4).contiguous()  # [B, H, 8*8*64/H, 5, 8*8]
-        unfold_v = unfold_v.permute(0, 2, 3, 1, 4).contiguous()  # [B, H, 8*8*64/H, 5, 8*8]
-
-        unfold_q = unfold_q.view(b, H, d, t * n)  # [B, H, 8*8*64/H, 5*8*8]
-        unfold_k = unfold_k.view(b, H, d, t * n)  # [B, H, 8*8*64/H, 5*8*8]
-        unfold_v = unfold_v.view(b, H, d, t * n)  # [B, H, 8*8*64/H, 5*8*8]
-
-        attn = torch.matmul(unfold_q.transpose(2, 3), unfold_k)  # [B, H, 5*8*8, 5*8*8]
-        attn = attn * (d ** (-0.5))  # [B, H, 5*8*8, 5*8*8]
-        attn = F.softmax(attn, dim=-1)  # [B, H, 5*8*8, 5*8*8]
-
-        attn_x = torch.matmul(attn, unfold_v.transpose(2, 3))  # [B, H, 5*8*8, 8*8*64/H]
-        attn_x = attn_x.view(b, H, t, n, d)  # [B, H, 5, 8*8, 8*8*64/H]
-        attn_x = attn_x.permute(0, 2, 1, 4, 3).contiguous()  # [B, 5, H, 8*8*64/H, 8*8]
-        attn_x = attn_x.view(b * t, D, n)  # [B*5, 8*8*64, 8*8]
-        feat = self.patch2feat(attn_x)  # [B*5, 64, 64, 64]
-
-        out = self.conv(feat).view(x.shape)  # [B, 5, 64, 64, 64]
-        out += x  # [B, 5, 64, 64, 64]
-
+        q = self.to_q(x.view(-1, c, h, w))  
+        k = self.to_k(x.view(-1, c, h, w))  
+        v = self.to_v(x.view(-1, c, h, w))  
+        unfold_q = self.feat2patch(q)  
+        unfold_k = self.feat2patch(k)  
+        unfold_v = self.feat2patch(v)  
+        unfold_q = unfold_q.view(b, t, H, d, n)  
+        unfold_k = unfold_k.view(b, t, H, d, n)  
+        unfold_v = unfold_v.view(b, t, H, d, n)  
+        unfold_q = unfold_q.permute(0, 2, 3, 1, 4).contiguous()  
+        unfold_k = unfold_k.permute(0, 2, 3, 1, 4).contiguous()  
+        unfold_v = unfold_v.permute(0, 2, 3, 1, 4).contiguous()  
+        unfold_q = unfold_q.view(b, H, d, t * n)  
+        unfold_k = unfold_k.view(b, H, d, t * n)  
+        unfold_v = unfold_v.view(b, H, d, t * n)  
+        attn = torch.matmul(unfold_q.transpose(2, 3), unfold_k) 
+        attn = attn * (d ** (-0.5))  
+        attn = F.softmax(attn, dim=-1) 
+        attn_x = torch.matmul(attn, unfold_v.transpose(2, 3)) 
+        attn_x = attn_x.view(b, H, t, n, d)  
+        attn_x = attn_x.permute(0, 2, 1, 4, 3).contiguous()  
+        attn_x = attn_x.view(b * t, D, n) 
+        feat = self.patch2feat(attn_x) 
+        out = self.conv(feat).view(x.shape)  
+        out += x 
         return out
 
 
